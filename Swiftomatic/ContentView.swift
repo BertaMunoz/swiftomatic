db067568f3c8c7048e08884795ac768b76f8368b
parispandas <divin.lixia@gmail.com>
2021-03-16 16:30:22 +0100

    add Tabview

--- Swiftomatic/ContentView.swift
+++ Swiftomatic/ContentView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            Text("")
+                .tabItem {
+                    VStack{
+                        Image(systemName:"applelogo")
+                        Text ("Frameworks")
+                    }.tag(1)
+                }
+            
+            Text("")
+                .tabItem {
+                    VStack{
+                        Image(systemName:"text.book.closed.fill")
+                    Text ("Livres")
+                    }.tag(2)
+                }
+            
+            Text("")
+                .tabItem {
+                    VStack{
+                        Image(systemName:"play.rectangle.fill")
+                    Text ("Conférences")
+                    }.tag(3)
+                }
+            Text("")
+                .tabItem {
+                    VStack{
+                        Image(systemName:"person.3.fill")
+                    Text ("Contributeurs")
+                    }.tag(4)
+                }
+                
+        }.font(.headline)
+        .accentColor(.orange)
     }
 }
 
