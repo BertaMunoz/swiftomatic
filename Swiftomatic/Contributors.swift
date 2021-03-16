736881d806588ddd0586ddb3e34295c862a6aeed
FrancoisCaron65 <francoiscaron65@gmail.com>
2021-03-16 17:00:06 +0100

    add contributors

---
+++ Swiftomatic/Contributors.swift
@@ -0,0 +1,22 @@
+//
+//  Contributors.swift
+//  Swiftomatic
+//
+//  Created by Naji Achkar on 16/03/2021.
+//
+
+import Foundation
+
+struct Contributor: Identifiable {
+    let id = UUID()
+    
+    var name: String = ""
+    var image: String = ""
+    var description: String = ""
+    
+    init(name: String, image: String, description: String) {
+        self.name = name
+        self.image = image
+        self.description = description
+    }
+}
