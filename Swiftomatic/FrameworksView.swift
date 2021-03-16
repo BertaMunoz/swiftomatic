3a1d1e09d834c619d804ed482e64722514d753b9
FrancoisCaron65 <francoiscaron65@gmail.com>
2021-03-16 15:33:41 +0100

    create FrameworksView

---
+++ Swiftomatic/FrameworksView.swift
@@ -0,0 +1,21 @@
+//
+//  FrameworksView.swift
+//  Swiftomatic
+//
+//  Created by François CARON on 16/03/2021.
+//
+
+import SwiftUI
+
+struct FrameworksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .foregroundColor(.purple)
+    }
+}
+
+struct FrameworksView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworksView()
+    }
+}
